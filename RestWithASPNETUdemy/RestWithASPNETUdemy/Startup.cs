115df42c17d646092d474a538eca0d165d82cdfd
Gustavo-Kuze <gustavoksilva3@hotmail.com>
2018-11-15 21:42:34 -0200

    GTPC

--- RestWithASPNETUdemy/RestWithASPNETUdemy/Startup.cs
+++ RestWithASPNETUdemy/RestWithASPNETUdemy/Startup.cs
@@ -44,7 +44,7 @@ namespace RestWithASPNETUdemy
         {
             if (env.IsDevelopment())
             {
-                app.UseDeveloperExceptionPage();
+                app.UseDeveloperExceptionPage(); 
             }
 
             app.UseMvc();
