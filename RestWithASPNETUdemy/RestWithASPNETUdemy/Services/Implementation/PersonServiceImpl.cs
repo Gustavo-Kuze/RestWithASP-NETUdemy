b29be642cc9e453cf8a6e9ea5229ac92eedc0a2d
Gustavo-Kuze <gustavoksilva3@hotmail.com>
2018-11-15 17:57:44 -0200

    IoC Dependecy Injection

---
+++ RestWithASPNETUdemy/RestWithASPNETUdemy/Services/Implementation/PersonServiceImpl.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using RestWithASPNETUdemy.Model;
+
+namespace RestWithASPNETUdemy.Services.Implementation
+{
+    public class PersonServiceImpl : IPersonService
+    {
+        public Person Create(Person person)
+        {
+            return person;
+        }
+
+        public void Delete(long id)
+        {
+           
+        }
+
+        public List<Person> FindAll()
+        {
+            return new List<Person>();
+
+        }
+
+        public Person FindById(long id)
+        {
+            return new Person() {
+                Id = 1,
+                Name = "Leandro",
+                LastName = "Costa",
+                Address = "Uberlândia",
+                Gender = "Male"
+
+            };
+        }
+
+        public Person Update(Person person)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
